c0df0266bea053445fdc4b0e25a6ff9466be551b
Gary Burgess <gary.burgess@gmail.com>
2015-03-14 16:10:10 +0000

    Add Document

--- src/DOM.purs
+++ src/DOM.purs
@@ -3,6 +3,9 @@ module DOM where
 -- | Effect type for DOM maniupulation
 foreign import data DOM :: !
 
+-- | General type for DOM documents.
+foreign import data Document :: *
+
 -- | General type for DOM nodes.
 foreign import data Node :: *
 
