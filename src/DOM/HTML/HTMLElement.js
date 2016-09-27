6b01deaa5e770555e26c5384dd66ce3a7f1c07f5
Nicholas Wolverson <nicholas.wolverson@gmail.com>
2016-09-27 20:40:26 +0100

    Add HTMLElement properties/methods

---
+++ src/DOM/HTML/HTMLElement.js
@@ -0,0 +1,201 @@
+"use strict";
+
+// ----------------------------------------------------------------------------
+
+exports.title = function (elt) {
+  return function () {
+    return elt.title;
+  };
+};
+
+exports.setTitle = function (title) {
+  return function (elt) {
+    return function () {
+      elt.title = title;
+      return {};
+    };
+  };
+};
+
+// ----------------------------------------------------------------------------
+
+exports.lang = function (elt) {
+  return function () {
+    return elt.lang;
+  };
+};
+
+exports.setLang = function (lang) {
+  return function (elt) {
+    return function () {
+      elt.lang = lang;
+      return {};
+    };
+  };
+};
+
+// ----------------------------------------------------------------------------
+
+exports.dir = function (elt) {
+  return function () {
+    return elt.dir;
+  };
+};
+
+exports.setDir = function (dir) {
+  return function (elt) {
+    return function () {
+      elt.dir = dir;
+      return {};
+    };
+  };
+};
+
+// ----------------------------------------------------------------------------
+
+exports.className = function (elt) {
+  return function () {
+    return elt.className;
+  };
+};
+
+exports.setClassName = function (className) {
+  return function (elt) {
+    return function () {
+      elt.className = className;
+      return {};
+    };
+  };
+};
+
+// ----------------------------------------------------------------------------
+
+exports.hidden = function (elt) {
+  return function () {
+    return elt.hidden;
+  };
+};
+
+exports.setHidden = function (hidden) {
+  return function (elt) {
+    return function () {
+      elt.hidden = hidden;
+      return {};
+    };
+  };
+};
+
+// ----------------------------------------------------------------------------
+
+exports.click = function (elt) {
+  return function () {
+    return elt.click();
+  };
+};
+
+// ----------------------------------------------------------------------------
+
+exports.tabIndex = function (elt) {
+  return function () {
+    return elt.tabIndex;
+  };
+};
+
+exports.setTabIndex = function (tabIndex) {
+  return function (elt) {
+    return function () {
+      elt.tabIndex = tabIndex;
+      return {};
+    };
+  };
+};
+
+// ----------------------------------------------------------------------------
+
+exports.hidden = function (elt) {
+  return function () {
+    return elt.hidden;
+  };
+};
+
+exports.setHidden = function (hidden) {
+  return function (elt) {
+    return function () {
+      elt.hidden = hidden;
+      return {};
+    };
+  };
+};
+
+// ----------------------------------------------------------------------------
+
+exports.focus = function (elt) {
+  return function () {
+    return elt.focus();
+  };
+};
+
+// ----------------------------------------------------------------------------
+
+exports.blur = function (elt) {
+  return function () {
+    return elt.blur();
+  };
+};
+
+// ----------------------------------------------------------------------------
+
+exports.draggable = function (elt) {
+  return function () {
+    return elt.draggable;
+  };
+};
+
+exports.setDraggable = function (draggable) {
+  return function (elt) {
+    return function () {
+      elt.draggable = draggable;
+      return {};
+    };
+  };
+};
+
+// ----------------------------------------------------------------------------
+
+exports.contentEditable = function (elt) {
+  return function () {
+    return elt.contentEditable;
+  };
+};
+
+exports.setContentEditable = function (contentEditable) {
+  return function (elt) {
+    return function () {
+      elt.contentEditable = contentEditable;
+      return {};
+    };
+  };
+};
+
+exports.isContentEditable = function (elt) {
+  return function () {
+    return elt.isContentEditable;
+  };
+};
+
+// ----------------------------------------------------------------------------
+
+exports.spellcheck = function (elt) {
+  return function () {
+    return elt.spellcheck;
+  };
+};
+
+exports.setSpellcheck = function (spellcheck) {
+  return function (elt) {
+    return function () {
+      elt.spellcheck = spellcheck;
+      return {};
+    };
+  };
+};
