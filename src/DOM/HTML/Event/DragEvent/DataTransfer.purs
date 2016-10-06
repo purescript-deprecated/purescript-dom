9cb163f0c38213561752d50ccd13fd6514f58b7f
Ryan L. Bell <ryan.l.bell@gmail.com>
2016-10-06 00:05:54 -0400

    Adds a function for getting types from a data transfer object

--- src/DOM/HTML/Event/DragEvent/DataTransfer.purs
+++ src/DOM/HTML/Event/DragEvent/DataTransfer.purs
@@ -7,3 +7,7 @@ foreign import data DataTransfer :: *
 -- | Contains a list of all the local files available on the data transfer.
 -- | Empty if the drag operation doesn't involve dragging files.
 foreign import files :: DataTransfer -> FileList
+
+-- | Returns an array of data formats used in the drag operation.
+-- | If the drag operation included no data, then the array is empty.
+foreign import types :: DataTransfer -> Array String
