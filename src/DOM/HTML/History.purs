d303bbe09baaf2aee602ee77714ac4f6f27d12fa
chexxor <chexxor@gmail.com>
2017-01-19 12:09:21 -0600

    Use Foreign instead of polymorphic a.

--- src/DOM/HTML/History.purs
+++ src/DOM/HTML/History.purs
@@ -1,7 +1,8 @@
 module DOM.HTML.History where
 
 import Control.Monad.Eff (Eff)
-import DOM.HTML.Types (HISTORY, History, Window)
+import DOM.HTML.Types (HISTORY, History)
+import Data.Foreign (Foreign)
 import Prelude (Unit)
 
 -- DocumentTitle will set value of `document.title`
@@ -12,6 +13,6 @@ newtype URL = URL String -- Unsure how to better type this.
 foreign import back :: forall e. History -> Eff (history :: HISTORY | e) Unit
 foreign import forward :: forall e. History -> Eff (history :: HISTORY | e) Unit
 foreign import go :: forall e. History -> Delta -> Eff (history :: HISTORY | e) Unit
-foreign import pushState :: forall a e. History -> a -> DocumentTitle -> URL -> Eff (history :: HISTORY | e) Unit
-foreign import replaceState :: forall a e. History -> a -> DocumentTitle -> URL -> Eff (history :: HISTORY | e) Unit
-foreign import state :: forall a e. History -> Eff (history :: HISTORY | e) a
+foreign import pushState :: forall e. History -> Foreign -> DocumentTitle -> URL -> Eff (history :: HISTORY | e) Unit
+foreign import replaceState :: forall e. History -> Foreign -> DocumentTitle -> URL -> Eff (history :: HISTORY | e) Unit
+foreign import state :: forall e. History -> Eff (history :: HISTORY | e) Foreign
