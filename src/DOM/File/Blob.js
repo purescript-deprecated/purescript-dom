44ea9da3333df65b465041bc1f305db2c0fca721
Irakli Safareli <i.safareli@gmail.com>
2017-09-27 17:59:08 +0200

    fix build errors

--- src/DOM/File/Blob.js
+++ src/DOM/File/Blob.js
@@ -4,12 +4,12 @@ exports.typeImpl = function (blob) { return blob.type; };
 
 exports.size = function (blob) { return blob.size; };
 
-exports._slice = function (contentType) {
+exports.slice = function (contentType) {
   return function (start) {
     return function (end) {
       return function (blob) {
         return blob.slice(start, end, contentType);
-      }
-    }
-  }
-}
+      };
+    };
+  };
+};
