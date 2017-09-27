a2182b1e9e6164d8d0e1efc93549f7ac4ca49a83
Irakli Safareli <i.safareli@gmail.com>
2017-09-27 17:46:09 +0200

    add Blob.slice

--- src/DOM/File/Blob.js
+++ src/DOM/File/Blob.js
@@ -3,3 +3,13 @@
 exports.typeImpl = function (blob) { return blob.type; };
 
 exports.size = function (blob) { return blob.size; };
+
+exports._slice = function (blob) {
+  return function (start) {
+    return function (end) {
+      return function (contentType) {
+        return blob.slice(start, end, contentType);
+      }
+    }
+  }
+}
