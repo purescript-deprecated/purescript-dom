23461ba532ab9295e382826f479ee6fe0a28e26f
Gary Burgess <gary.burgess@gmail.com>
2017-10-26 18:54:14 +0100

    Add document readyState
    And make it so test failures actually fail the tests

--- src/DOM/HTML/Document.purs
+++ src/DOM/HTML/Document.purs
@@ -1,15 +1,26 @@
 module DOM.HTML.Document
   ( body
+  , readyState
+  , module Exports
   ) where
 
 import Prelude
+
 import Control.Monad.Eff (Eff)
-import Data.Maybe (Maybe)
-import Data.Nullable (Nullable, toMaybe)
 import DOM (DOM)
+import DOM.HTML.Document.ReadyState (ReadyState(..)) as Exports
+import DOM.HTML.Document.ReadyState (ReadyState, parseReadyState)
 import DOM.HTML.Types (HTMLElement, HTMLDocument)
+import Data.Maybe (Maybe, fromJust)
+import Data.Nullable (Nullable, toMaybe)
+import Partial.Unsafe (unsafePartial)
 
 foreign import _body :: forall eff. HTMLDocument -> Eff (dom :: DOM | eff) (Nullable HTMLElement)
 
 body :: forall eff. HTMLDocument -> Eff (dom :: DOM | eff) (Maybe HTMLElement)
 body = map toMaybe <<< _body
+
+foreign import _readyState :: forall eff. HTMLDocument -> Eff (dom :: DOM | eff) String
+
+readyState :: forall eff. HTMLDocument -> Eff (dom :: DOM | eff) ReadyState
+readyState = map (unsafePartial fromJust <<< parseReadyState) <<< _readyState
--- test/Main.purs
+++ test/Main.purs
@@ -1,32 +1,20 @@
 module Test.Main where
 
-import Prelude (($), discard)
-import Control.Monad.Aff (launchAff, Canceler)
+import Prelude
+
 import Control.Monad.Aff.AVar (AVAR)
+import Control.Monad.Aff.Console (CONSOLE)
 import Control.Monad.Eff (Eff)
-import Control.Monad.Eff.Class (liftEff)
-import Control.Monad.Eff.Console (CONSOLE)
-import Control.Monad.Eff.Exception (EXCEPTION)
 import DOM (DOM)
 import DOM.HTML.Types (WINDOW)
-import Data.Enum (fromEnum)
-import ExitCodes (ExitCode(Success))
-import PhantomJS.Phantom (exit, PHANTOMJS)
+import Test.DOM.HTML.Document (domHtmlDocumentTests)
 import Test.DOM.HTML.Window (domHtmlWindowTests)
 import Test.DOM.Node.DOMTokenList (domTokenListTests)
-import Test.Unit (describe, it)
-import Test.Unit.Assert (assert)
-import Test.Unit.Output.Simple (runTest)
+import Test.Unit.Console (TESTOUTPUT)
+import Test.Unit.Main (runTest)
 
-main
-  :: forall eff
-   . Eff (exception :: EXCEPTION, console :: CONSOLE, avar :: AVAR, dom :: DOM, window :: WINDOW, phantomjs :: PHANTOMJS | eff)
-         (Canceler (console :: CONSOLE, avar :: AVAR, dom :: DOM, window :: WINDOW, phantomjs :: PHANTOMJS | eff))
-main = launchAff $ runTest do
+main :: forall eff. Eff (console :: CONSOLE, testOutput :: TESTOUTPUT, avar :: AVAR, dom :: DOM, window :: WINDOW | eff) Unit
+main = runTest do
+  domHtmlDocumentTests
   domHtmlWindowTests
   domTokenListTests
-
-  describe "exit" $ do
-    it "should exit" $ do
-      liftEff $ exit (fromEnum Success)
-      assert "failed to exit phantomjs" false
